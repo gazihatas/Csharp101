4a8dd48c57a1943cde4f85947a4a902d10ddf593
Gazi Hataş <gazihatas71@gmail.com>
2022-07-17 14:44:55 +0300

    while-foreach

---
+++ donguler-while-foreach/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace donguler_while_foreach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //while
+            //koşul true olduğu sürece kod bloğu çalışır
+            //1 den başlayarak console dan girilen sayıya kadar (sayı dahil) ortalama hesaplayıp console a yazdıran program.
+            Console.WriteLine("Lütfen bir sayı giriniz");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while (sayac <= sayi)
+            {
+                toplam += sayac;
+                sayac++;
+            }
+            Console.WriteLine(toplam/sayi);
+
+            //'a' dan 'z' ye kadar tüm harfleri console a yazdır.
+            char character = 'a';
+            while (character<'z')
+            {
+                Console.Write(character);
+                character++;
+            }
+
+            //foreach
+            Console.Write("***** Foreach *****");
+            string[] arabalar = { "BMW", "Ford", "Toyota", "Nissan" };
+
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+        }
+    }
+}
