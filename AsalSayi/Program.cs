386f3560d8a9e5ffffbc9f1ce0a414ea603447aa
Gazi Hataş <gazihatas71@gmail.com>
2022-08-17 11:34:32 +0300

    Soru 1

---
+++ AsalSayi/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data.Common;
+using System.Runtime.CompilerServices;
+
+namespace AsalSayi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (IsPrimeNumber(6))
+            {
+                Console.WriteLine("This is a prime number");
+            }
+            else
+            {
+                Console.WriteLine("Asal sayı değildir");
+            }
+
+            Console.ReadLine();
+        }
+
+        private static bool IsPrimeNumber(int number)
+        {
+
+            //kendisi ve  hariç tam böleni olmayan sayıdır.
+
+            bool result = true;
+
+            for (int i = 2; i<number-1; i++)
+            {
+                if (number%i==0)
+                {
+                    result = false;
+                    i = number;
+                }
+            }
+            return result;
+        }
+    }
+}
