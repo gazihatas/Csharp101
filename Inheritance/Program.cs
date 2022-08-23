6c692b6ac62c6d095ee2dd6487ba7b5880a088ad
Gazi Hataş <gazihatas71@gmail.com>
2022-08-23 14:53:21 +0300

    Inheritance

---
+++ Inheritance/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Inheritance
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Bitkiler.TohumluBitkiler tohumluBitki = new Bitkiler.TohumluBitkiler();
+            tohumluBitki.TohumlaCogalma();
+
+            Console.WriteLine("**************");
+
+            Kuslar martı = new Kuslar();
+            martı.Ucmak();
+
+
+        }
+    }
+}
