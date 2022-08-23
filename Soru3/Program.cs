bed96252b8dd26917e4c1ac90d59b891ebd8c0ca
Gazi Hataş <gazihatas71@gmail.com>
2022-08-23 10:16:00 +0300

    Soru 3

---
+++ Soru3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Soru3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a sentence");
+            string? sentence = Console.ReadLine();
+            List<char> vowels = new List<char>() { 'a', 'e', 'ı', 'i', 'o', 'ö', 'u', 'ü' };
+            int vowelsCount = vowels.Count;
+            string sentenceVowels = "";
+
+            if (!String.IsNullOrEmpty(sentence))
+            {
+                for (int i = 0; i < sentence.Length; i++)
+                {
+                    for (int j = 0; j < vowelsCount; j++)
+                    {
+                        if (char.ToLower(sentence[i]) == vowels[j])
+                        {
+                            sentenceVowels += sentence[i];
+                            vowels.Remove(vowels[j]);
+                            vowelsCount--;
+                        }
+                    }
+                }
+            }
+            else
+                Console.WriteLine("Sentence can not be a null value");
+
+            if (!string.IsNullOrEmpty(sentence))
+            {
+                char[] vowelArray = sentenceVowels.ToArray();
+                Array.Sort(vowelArray);
+
+                for (int i = 0; i < vowelArray.Length; i++)
+                    Console.WriteLine(vowelArray[i]);
+            }
+        }
+    }
+}
